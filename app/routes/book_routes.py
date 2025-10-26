2fcf02927abc08a59e999ce6b43640350b4c9135
Xin Li <happyxin331@icloud.com>
2025-10-26 15:24:39 -0700

    Read, Update, and Delete

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request,Response
 from app.models.book import Book
 from ..db import db
 
@@ -17,14 +17,13 @@ def create_book():
     response = {
         "id": new_book.id,
         "title": new_book.title,
-        "description": new_book.description,
+        "description": new_book.description
     }
     return response, 201
 
 @books_bp.get("")
 def get_all_book():
     query = db.select(Book).order_by(Book.id)
-    print(query)
     books = db.session.scalars(query)
 
     books_response = []
@@ -38,7 +37,51 @@ def get_all_book():
         )
     return books_response
 
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book = validate_book(book_id)
+  
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
+    
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response, 400))
 
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query)
+
+    if not book:
+        response = {"message": f"book {book_id} not found"}
+        abort(make_response(response, 400))
+
+    return book
+
+@books_bp.put("/<book_id>")
+def update_book(book_id):
+    book = validate_book(book_id)
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+@books_bp.delete("/<book_id>")
+def delete_book(book_id):
+    book = validate_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
 # @books_bp.get("")
 # def get_all_books():
 #     books_response = []
